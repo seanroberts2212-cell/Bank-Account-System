976ea19ed6023f0fcf431841e0dc559a46d227bc
seanroberts2212-cell <seanroberts2212@gmail.com>
2026-05-06 12:23:41 +0100

    Implement CheckingAccount class with overdraft feature

---
+++ CheckingAccount.java
@@ -0,0 +1,22 @@
+public class CheckingAccount extends Account {
+    private double overdraft;
+
+    public CheckingAccount(String customerName, double balance, double overdraft) {
+        super(customerName, balance);
+        this.overdraft = overdraft;
+    }
+    public double getOverdraft() {
+        return overdraft;
+    }
+    public void setOverdraft(double overdraft) {
+        this.overdraft = overdraft;
+    }
+    public void withdraw(double amount) {
+        
+        if(amount <= balance + overdraft) {
+            balance = balance - amount;
+        } else {
+            System.out.println("Exceeds overdraft limits");
+        }
+    }
+}
